2da99c9b11d6d7ee087bc00e0d78db4ef2472a62
Maratah <maratah7@gmail.com>
2020-07-04 14:36:42 +0300

    add input to the configuration .py file to fit the nees for the apllication, each defined in classes.

---
+++ app/config.py
@@ -0,0 +1,26 @@
+# configuration
+class Config:
+    '''
+    General Configuration parent class
+    '''
+    pass
+
+class ProdCofig(Config):
+    '''
+    Child production configution class
+  
+    Args:
+        Config:The parent configuration class with genaral configuration settings
+    '''
+    pass 
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration with General configuration settings
+
+    '''
+
+         
