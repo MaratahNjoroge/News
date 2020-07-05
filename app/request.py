87afeb9d0af5ea92f738a3e283c15af2f0ff07a5
Maratah <maratah7@gmail.com>
2020-07-05 14:25:38 +0300

    add a function that takes in blog name to search for blogs and returns the seacrhed resuls.

--- app/request.py
+++ app/request.py
@@ -46,15 +46,51 @@ def process_results(blog_list):
         name = blog_item.get('name')
         author = blog_item.get('author')
         description = blog_item.get('description')
-        publishedAt = blog_item.get('date_published')
+        published = blog_item.get('date_published')
         content = blog_item.get('blog_content')
        
 
         if content:
-            blog_object = Blog(id,title,name,author,description,publishedAt,content)
+            blog_object = Blog(id,title,name,author,description,published,content)
             blog_results.append(blog_object) 
 
     return blog_results
     
+def get_blog(id): #function takes in blog id to return a blog object
+    get_blog_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_blog_details_url) as url:
+        blog_details_data = url.read()
+        blog_details_response = json.loads(blog_details_data)
+
+        blog_object = None
+        if blog_details_response.get('id'):
+            id = blog_details_response.get('id')
+            title = blog_details_response.get('title')
+            name =  blog_details_response.get('name')
+            author = blog_details_response.get('author')
+            description = blog_details_response.get('description')
+            published = blog_details_response.get('date_published')
+            content = blog_details_response.get('blog_content')
+
+            blog_object = Blog (id,title,name,author,description,published,content)
+
+    return blog_object
+
+def search_blog(blog_name):
+    search_blog_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}' .format(api_key,blog_name)
+    with urllib.request.urlopen(search_blog_url) as url:
+        search_blog_data = url.read()
+        search_blog_response = json.loads(search_blog_data)
+
+        search_blog_results = None
+
+        if search_blog_response['results']:
+            search_blog_list = search_blog_response['results']
+            search_blog_results= process_results(search_blog_list)
+
+    return search_blog_results
+
+            
 
     
