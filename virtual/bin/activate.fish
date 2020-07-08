df28d723dd55bff65851172f8bebbe6c6aa0a131
Maratah <maratah7@gmail.com>
2020-07-08 17:15:55 +0300

    commit modified files.

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/.local/share/Trash/files/Blog-post/Blog-post/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/Blog-post/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
